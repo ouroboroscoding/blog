e3f0f639e1e1044ffd7335bef412f3e5fb93424e
Chris Nasr <chris@ouroboroscoding.com>
2026-04-10 15:15:46 -0400

    Hotfix 0.3.1 - Incremented patch version - Fixed a bug where renaming the slug of a post caused a crash - Fixed bugs related to missed parts of upgrading OC - Added two new upgrade scripts to go from previous versions

---
+++ blog/upgrades/v0_3_0_v0_3_1.py
@@ -0,0 +1,65 @@
+# coding=utf8
+""" Upgrade 0.3.0 to 0.3.1
+
+Handles taking the existing 0.3.0 data and converting it to a usable format in
+0.3.1
+"""
+
+__author__		= "Chris Nasr"
+__copyright__	= "Ouroboros Coding Inc."
+__version__		= "1.0.0"
+__email__		= "chris@ouroboroscoding.com"
+__created__		= "2025-11-18"
+
+# Ouroboros imports
+from config import config
+import jsonb
+from rest_mysql.Record_MySQL import Commands, DuplicateException, ESelect
+from strings import uuid_strip_dashes
+
+# Python imports
+from os import makedirs
+from os.path import abspath, expanduser, exists
+
+# Pip imports
+from pymysql.converters import escape_string
+
+# Local imports
+from blog.records import media
+
+def run():
+	"""Run
+
+	Main entry into the script, called by the upgrade module
+
+	Returns:
+		bool
+	"""
+
+	# Get the media structure
+	dS = media.Media.struct()
+
+	# Get the select
+	sFields = media.Media.provide_select([ '_id', 'image' ])
+
+	# Fetch all the media records
+	lRecords = Commands.select(
+		dS['host'],
+		f"SELECT {sFields} FROM `{dS['db']}`.`{dS['table']}`"
+	)
+
+	# Go through each record
+	for d in lRecords:
+
+		# Convert the string to a string
+		d['image'] = jsonb.decode(d['image'])
+
+		# Update the record
+		Commands.execute(dS['host'], (
+			f"UPDATE `{dS['db']}`.`{dS['table']}` "
+			f"SET `image` = '{d['image']}' "
+			f"WHERE `_id` = UNHEX('{d['_id']}')"
+		))
+
+	# Return OK
+	return True
